8c91d199851b8cc1ca4731f1496a7502c8c385fe
ujnss <unlinunlin@163.com>
2021-04-28 12:20:06 +0800

    update perf

--- system_info.sh
+++ system_info.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/env bash
 
-########################################
-# CPU & Memory Info
-# name/logical/physical/cores
-cpu_n=$(cat /proc/cpuinfo | grep "model name" | head -n 1 | awk -F":" '{print $2}')
-cpu_l=$(cat /proc/cpuinfo | grep "processor" | wc -l)
-cpu_p=$(cat /proc/cpuinfo | grep "physical id" | sort | uniq | wc -l)
-
-echo -e "\n>>>>>>>>>>> CPU Info <<<<<<<<<<<"
-echo " Model Name:$cpu_n"
-echo "Physical(s): $cpu_p"
-echo " Logical(s): $cpu_l"
-
-echo -e "\n>>>>>>>>>>> MEM Info <<<<<<<<<<<"
-mem_info=($(awk '/MemTotal/{memtotal=$2}/MemAvailable/{memavailable=$2}END{printf "%.3f %.3f %.2f",memtotal/1024/1024," "(memtotal-memavailable)/1024/1024," "(memtotal-memavailable)/memtotal*100}' /proc/meminfo))
-echo total:${mem_info[0]}G used:${mem_info[1]}G Usage:${mem_info[2]}%
+##################################################
 echo ""
+echo "     Date Time: $(date)"
+echo "Operate System: $(uname -a)"
+##################################################
+echo -e "\n>>>>>>>>>>>>>> CPU Info <<<<<<<<<<<<<<"
+physical_cpus=$(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)
+logical_cpus=$(grep "processor" /proc/cpuinfo | wc -l)
+cpu_kernels=$(grep "cores" /proc/cpuinfo | uniq | awk -F ': ' '{print $2}')
+cpu_type=$(grep "model name" /proc/cpuinfo | awk -F ': ' '{print $2}' | sort | uniq)
+cpu_arch=$(uname -m)
+echo "Physical(s): $physical_cpus"
+echo " Logical(s): $logical_cpus"
+echo "  Cores/CPU: $cpu_kernels"
+echo "       Type: $cpu_type"
+echo "       Arch: $cpu_arch"
+##################################################
+echo -e "\n>>>>>>>>>>>>>> Mem Info <<<<<<<<<<<<<<"
+free -h
+##################################################
+echo -e "\n>>>>>>>>>>>>> Disk Info <<<<<<<<<<<<<<"
+df -hiP | sed 's/Mounted on/Mounted/' >/tmp/inode
+df -hTP | sed 's/Mounted on/Mounted/' >/tmp/disk
+join /tmp/disk /tmp/inode | awk '{print $1,$2,"|",$3,$4,$5,$6,"|",$8,$9,$10,$11,"|",$12}' | column -t
 ########################################
+echo ""
